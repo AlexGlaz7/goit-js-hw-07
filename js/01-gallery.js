9c99ef4e7e8cf8e8b97be6b76770be5843175d46
Alexandr Glazunov <matveychuk1998@gmail.com>
2023-01-19 13:28:35 +0200

    Connected libraries and completed the task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const divRef = document.querySelector(".js-gallery");
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <div class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+        </a>
+      </div>
+    `;
+    })
+    .join("");
+}
+
+const addGalleryMarkup = createGalleryMarkup(galleryItems);
+divRef.innerHTML = addGalleryMarkup;
+divRef.addEventListener("click", onImageClick);
+
+function onImageClick(evt) {
+  blockStandartAction(evt);
+
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+      <img src="${evt.target.dataset.source}" width="800" height="600"/>
+  `);
+
+  instance.show();
+
+  divRef.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+}
+
+function blockStandartAction(evt) {
+  evt.preventDefault();
+}
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const divRef = document.querySelector(".js-gallery");
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map((item) => {
+      return `
+        <a class="gallery__link" href="${item.original}">
+          <img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
+        </a>
+    `;
+    })
+    .join("");
+}
+
+const addGalleryMarkup = createGalleryMarkup(galleryItems);
+divRef.innerHTML = addGalleryMarkup;
+
+const gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+});
+
+console.log(galleryItem);
